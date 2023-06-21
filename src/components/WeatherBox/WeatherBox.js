03e21005cb9f7a291d77aab6020fb31635739c2e
Karolina Hyla <karatakarabe@gmail.com>
2023-06-21 14:37:58 +0200

    Add wroking api

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,13 +1,45 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
+import { useCallback,useState, useRef } from 'react';
 
 const WeatherBox = props => {
+  const [pending, setPending] = useState(false)
+  const [error, setError] = useState(false)
+  const APIkey = ""
+
+  const weatherData = useRef()
+  const handleCityChange = useCallback(city =>{
+    setPending(true)
+    setError(false)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`)
+    .then(res => {
+      if(res.status === 200) {
+        return res.json()
+          .then(data => {
+            weatherData.current = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main}
+            console.log(data, weatherData)
+            setPending(false)
+          })
+        } else {
+          setPending(false)
+          setError(true)
+        }
+    })
+  }, []) 
+  
+
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
-      <Loader />
+      <PickCity action={handleCityChange}/>
+      {(weatherData.current && !pending && !error) && <WeatherSummary data={weatherData.current}/>}
+      {pending && <Loader />}
+      {error && <ErrorBox />}
     </section>
   )
 };
